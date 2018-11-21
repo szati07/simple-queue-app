ba0443f6fdd9ed6b3c8d9e31a7e9a37ed2677191
Bence Szatmári <szati195@gmail.com>
2018-11-21 23:29:58 +0100

    added queue

--- node_app1/index.js
+++ node_app1/index.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -13,8 +27,12 @@ http.listen(3000, () => {
     console.log('Listening on *:3000')
 });
 
-io.on('connection', function (socket) {
+const socketIds = [];
+
+io.on('connection', (socket) => {
     socket.on('Job Request', () => {
-        console.log('Valami bejott')
-    })
+        axios.get('http://localhost:3001/test1').then((resp) => {
+            socket.emit('feedback', { feedback: resp.data })
+        });
+    });
 });
--- node_app1/scripts/main.js
+++ node_app1/scripts/main.js
@@ -1,7 +1,12 @@
 {
-    const socket = io();
+    const socket = io('http://localhost:3000');
 
     $('button').on('click', () => {
         socket.emit('Job Request')
+    })
+
+    socket.on('feedback', (msg) => {
+        console.log(msg);
+        $('p').text(msg.feedback)
     })
 }
