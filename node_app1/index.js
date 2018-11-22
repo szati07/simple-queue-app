f2eab177af8c574cd98a352e62f8268f79518c8b
Bence Szatmari <bence_szatmari@epam.com>
2018-11-22 17:35:33 +0100

    added custom queue

--- node_app1/index.js
+++ node_app1/index.js
@@ -22,13 +22,52 @@ http.listen(80, () => {
     console.log('Listening on *:80')
 });
 
+function Queue() {
+    this.data = [];
+}
+
+Queue.prototype.add = function(record) {
+    this.data.push(record);
+  }
+  
+Queue.prototype.remove = function() {
+    this.data.shift();
+}
+
+Queue.prototype.first = function() {
+    return this.data[0];
+}
+  
+Queue.prototype.last = function() {
+    return this.data[this.data.length - 1];
+}
+  
+Queue.prototype.size = function() {
+    return this.data.length;
+}
+
+const q = new Queue();
+
+function callEndPoint(socket) {
+    if(q.first() === socket){
+        console.log('hello')
+        axios.get('http://localhost:81/queue-app')
+        .then((resp) => {
+            socket.emit('feedback', { feedback: resp.data })
+            socket.emit('enable');
+            q.remove();
+            if(q.size() > 0) {
+                console.log('Works like a charm');
+                callEndPoint(q.first());
+            }
+        });
+    }
+}
+
 io.on('connection', (socket) => {
     socket.on('Job Request', () => {
         socket.emit('disable', {disable: true});
-        axios.get('http://localhost:81/queue-app')
-            .then((resp) => {
-                socket.emit('feedback', { feedback: resp.data })
-                socket.emit('enable');
-        });
+        q.add(socket);
+        callEndPoint(socket); 
     });
 });
