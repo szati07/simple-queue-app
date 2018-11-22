ebc113e25c76bb06f3ff0861551ab6ceb5bdb9cb
Bence Szatmari <bence_szatmari@epam.com>
2018-11-22 17:36:45 +0100

    removed mw from queue

--- node_app2/queue.js
+++ node_app2/queue.js
@@ -3,13 +3,6 @@ const app = express();
 const http = require('http').Server(app);
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const expressQueue = require('express-queue');
-
-const queueMw = expressQueue({
-    activeLimit: 1,
-    queuedLimit: -1
-});
-app.use(queueMw)
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -21,17 +14,9 @@ app.use(cors());
 
 const RESPONSE_DELAY = 5000;
 
-let counter = 0;
-
 app.get('/queue-app', (req, res) => {
-    let cnt = counter++;
-
-    console.log(`get(test1): [request] queueLength: ${queueMw.queue.getLength()}`);
-
     setTimeout(() => {
-        console.log(`get(test1): [${cnt}/ready] queueLength: ${queueMw.queue.getLength()}`);
         res.status(200).send('Request Accepted');
-        console.log(`get(test1): [${cnt}/sent] queueLength: ${queueMw.queue.getLength()}`);
     }, RESPONSE_DELAY)
 });
 
